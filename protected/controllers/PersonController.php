fc5ff5f577d124be9d9f503836f2af9b0c7634e4
Sailendra Shakya <sshakya@lftechnology.com>
2013-05-11 10:15:08 +0545

    added controller and view files

---
+++ protected/controllers/PersonController.php
@@ -0,0 +1,19 @@
+<?php
+
+class PersonController extends Controller
+{
+	public $layout = "//layouts/main";
+
+
+	/**
+	 * This is the default 'index' action that is invoked
+	 * when an action is not explicitly requested by users.
+	 */
+	public function actionIndex()
+	{
+		// renders the view file 'protected/views/site/index.php'
+		// using the default layout 'protected/views/layouts/main.php'
+		$this->render('index');
+	}
+
+}
---
+++ protected/views/project/index.php
@@ -0,0 +1,7 @@
+<?php
+echo "project";
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
