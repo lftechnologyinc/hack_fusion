8e647ddb608f55184dc347dcf04050cca2d4000d
Sailendra Shakya <sshakya@lftechnology.com>
2013-05-11 16:30:07 +0545

    add

---
+++ protected/views/site/slider.php
@@ -0,0 +1,89 @@
+<section class="slider" style="clear: both;height: 330px;margin-top: 70px;overflow-y: scroll; background:#FFF">
+                                <div class="flexslider">
+                                  <ul class="slides">
+                                    <li>
+                                                        <div style="clear: both; display: block; float: left; width: 100%;">
+                                                                <div style="float:left; margin-top:10px">
+                                                                        <i class="icon_edit"></i>
+                                                                        <i class="icon_delete" style="margin-left:15px"></i>
+                                                                </div>
+                                                                <div style="float:right; margin:15px 2px">
+                                                                        <a class="add_btn" href="#">Add Section</a>
+                                                                </div>
+                                                                <h1 align="center" style="line-height:12px">Developer's Room</h1>
+                                                        </div>
+                                                        <div style="margin-top:15px; float:left; width:100%">
+                                                                <div style="float:left;">
+                                                                        <h2>Section 1</h2>
+                                                                </div>
+                                                                <div style="float:right; margin-top:10px">
+                                                                        <i class="icon_edit"></i>
+                                                                        <i class="icon_delete" style="margin-left:15px"></i>
+                                                                </div>
+                                                        </div>
+                                                        <hr class="divider">
+                                                        <ul class="list_people">
+                                                                <li>
+                                                                        <img src="images/avtar.png">
+                                                                        <h2>Bala Maharjan</h2>
+                                                                </li>
+                                                                <li>
+                                                                        <img src="images/avtar.png">
+                                                                        <h2>Bala Maharjan</h2>
+                                                                </li>
+                                                                <li>
+                                                                        <img src="images/avtar.png">
+                                                                        <h2>Bala Maharjan</h2>
+                                                                </li>
+                                                                <li>
+                                                                        <img src="images/avtar.png">
+                                                                        <h2>Bala Maharjan</h2>
+                                                                </li>
+                                                        </ul>
+                                        </li>
+                                        <li>
+
+                                                        <div style="clear: both; display: block; float: left; width: 100%;">
+                                                                <div style="float:left; margin-top:10px">
+                                                                        <i class="icon_edit"></i>
+                                                                        <i class="icon_delete" style="margin-left:15px"></i>
+                                                                </div>
+                                                                <div style="float:right; margin:15px 2px">
+                                                                        <a class="add_btn" href="#">Add Section</a>
+                                                                </div>
+                                                                <h1 align="center" style="line-height:12px">Developer's Room</h1>
+                                                        </div>
+                                                        <div style="margin-top:15px; float:left; width:100%">
+                                                                <div style="float:left;">
+                                                                        <h2>Section 1</h2>
+                                                                </div>
+                                                                <div style="float:right; margin-top:10px">
+                                                                        <i class="icon_edit"></i>
+                                                                        <i class="icon_delete" style="margin-left:15px"></i>
+                                                                </div>
+                                                        </div>
+                                                        <hr class="divider">
+                                                        <ul class="list_people">
+                                                                <li>
+                                                                        <img src="images/avtar.png">
+                                                                        <h2>Bala Maharjan</h2>
+                                                                </li>
+                                                                <li>
+                                                                        <img src="images/avtar.png">
+                                                                        <h2>Bala Maharjan</h2>
+                                                                </li>
+                                                                <li>
+                                                                        <img src="images/avtar.png">
+                                                                        <h2>Bala Maharjan</h2>
+                                                                </li>
+                                                                <li>
+                                                                        <img src="images/avtar.png">
+                                                                        <h2>Bala Maharjan</h2>
+                                                                </li>
+                                                        </ul>
+                                        </li>
+
+
+                                  </ul>
+                                </div>
+                              </section>
