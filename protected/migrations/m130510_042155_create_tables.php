2665c3f56c09acbc8fc2440ef08835b1fbd02669
Sailendra Shakya <sshakya@lftechnology.com>
2013-05-11 09:26:26 +0545

    initial database file

--- protected/migrations/m130510_042155_create_tables.php
+++ protected/migrations/m130510_042155_create_tables.php
@@ -5,14 +5,46 @@ class m130510_042155_create_tables extends CDbMigration
 
 	public function up()
 	{
-		$this->createTable('table_name', array(
+		$this->createTable('room', array(
 			'id' => 'int(11) NOT NULL AUTO_INCREMENT',
 			'name' => 'varchar(255) DEFAULT NULL',
-			'type' => "enum('general','bug','feature') DEFAULT NULL",
-			'created_at' => 'datetime DEFAULT NULL',
-			'updated_at' => 'varchar(45) DEFAULT NULL',
 			'PRIMARY KEY (`id`)'
 				), 'ENGINE InnoDB');
+
+		$this->createTable('section', array(
+			'id' => 'int(11) NOT NULL AUTO_INCREMENT',
+			'size' => 'int(3) DEFAULT NULL',
+			'room_id'=>'int(11) NOT NULL',
+			'PRIMARY KEY (`id`)'
+				), 'ENGINE InnoDB');
+
+		$this->createTable('person', array(
+			'id' => 'int(11) NOT NULL AUTO_INCREMENT',
+			'name' => 'varchar(255) DEFAULT NULL',
+			'manual_assign' => 'tinyint(1) DEFAULT 0',
+			'section_id'=>'int(11) NOT NULL',
+			'PRIMARY KEY (`id`)'
+				), 'ENGINE InnoDB');
+
+		$this->createTable('project', array(
+			'id' => 'int(11) NOT NULL AUTO_INCREMENT',
+			'name' => 'varchar(255) DEFAULT NULL',
+			'PRIMARY KEY (`id`)'
+				), 'ENGINE InnoDB');
+
+		$this->createTable('project_person', array(
+			'id' => 'int(11) NOT NULL AUTO_INCREMENT',
+			'person_id'=>'int(11) NOT NULL',
+			'project_id'=>'int(11) NOT NULL',
+			'PRIMARY KEY (`id`)'
+				), 'ENGINE InnoDB');
+
+		$this->addForeignKey('fk_section1', 'section', 'room_id', 'room', 'id', 'CASCADE', 'CASCADE');
+		$this->addForeignKey('fk_section2', 'person', 'section_id', 'section', 'id');
+
+		$this->addForeignKey('fk_section3', 'project_person', 'person_id', 'person', 'id', 'CASCADE', 'CASCADE');
+		$this->addForeignKey('fk_section4', 'project_person', 'project_id', 'project', 'id', 'CASCADE', 'CASCADE');
+
 	}
 
 	public function down()
